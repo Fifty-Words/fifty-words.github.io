481bb023fc33fbc0ba423bdcc99710160966dcaa
Matthew Spence <admin@matt-spence.com>
2021-08-09 21:24:20 +0100

    feat: setup basic Next JS framework (stripped out defaults)

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Fifty Words</title>
+      </Head>
+
+      <main>
+        {/* TODO ... */}
+      </main>
+
+      <footer>
+        {/* TODO ... */}
+      </footer>
+    </div>
+  )
+}
